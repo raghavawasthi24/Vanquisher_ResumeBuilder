6348efee495c6fc03580a38076cd22421a4ff298
Kanan-Agrawal <kanan.agrawal17@gmail.com>
2023-01-20 16:07:27 +0530

    settings

--- ResumeB/ResumeB/settings.py
+++ ResumeB/ResumeB/settings.py
@@ -75,23 +75,12 @@ WSGI_APPLICATION = 'ResumeB.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '2hiYOEvnXc5JfNBswLvL',
+        'HOST': 'containers-us-west-136.railway.app',
+        'PORT': '7393',
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
