15f63f1e76b9379e0a650c0e07a2a88b04dbd607
AnubhaSharma2709 <anubhaa.sharma27@gmail.com>
2023-01-20 21:28:03 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <React.StrictMode>
+     
       <App />
     </React.StrictMode>
   </BrowserRouter>
